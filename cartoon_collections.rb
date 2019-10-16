0ffd5225a7dcae4f40ec1ac74a94d8e0a6c90ece
cool-changelog-3932 <ruizj3@uw.edu>
2019-10-16 00:57:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,9 @@
+dwarves = %w["Doc", "Dopey", "Bashful", "Grumpy"]
 def roll_call_dwarves# code an argument here
   # Your code here
+  dwarves.each_with_index do |dwarf, index|
+    puts "#{index + 1}. #{dwarf}"
+  end  
 end
 
 def summon_captain_planet# code an argument here
