426c91a4fb39e2901a237f85a688954672f19409
cool-changelog-3932 <ruizj3@uw.edu>
2019-10-16 01:06:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,6 @@ def long_planeteer_calls(planeteer_calls)# code an argument here
     calls.length > 4
   end
 end
-end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
